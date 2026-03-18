297c26901a6ef02d278b467b4785bb9a19e9476e
commandblocks0 <dario.riapos0@gmail.com>
2026-03-18 07:12:20 +0100

    pwa

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,10 @@ self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open("app-cache").then((cache) => {
             return cache.addAll([
-                "index.html",
-                "style.css",
-                "script.js"
+                "./"
+                "./index.html",
+                "./style.css",
+                "./script.js"
             ]);
         })
     );
